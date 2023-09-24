ce495b66ebe8c546c58d7bc43cd22a546f47cdb1
nikakaraseva <nikarasevas@gmail.com>
2023-09-24 18:38:35 +0300

    last commit

---
+++ pytests.py
@@ -0,0 +1,29 @@
+import pytest
+from student import CheckName, Student
+
+@pytest.fixture
+def ch1():
+    return CheckName()
+
+@pytest.fixture
+def s1():
+    return Student('Ivan', 'Ivanov', 'Ivanovich', 'sub.csv')
+
+
+def test_check_name(ch1):
+    assert ch1.validate('Ivanov') == 'Ivanov', 'Test nom.1 failed'
+
+
+def test_open_csv(s1):
+    assert s1.sub_from_csv('sub.csv') == ['math', 'English', 'physics', 'biology', 'chemistry', 'history']
+
+
+def test_add_grade(s1):
+    s1.add_grades('math', 2)
+    assert s1.grades == {'math': [2], 'English': [], 'physics': [], 'biology': [], 'chemistry': [], 'history': []}
+
+
+
+if __name__ == '__main__':
+    pytest.main(['-vv'])
+
--- unittests.py
+++ unittests.py
@@ -7,7 +7,7 @@ class TestStudent(unittest.TestCase):
     def setUp(self) -> None:
         self.ch1 = CheckName()
         self.s1 = Student('Ivan', 'Ivanov', 'Ivanovich', 'sub.csv')
-        self.grades = {'math': [3, 4], 'English': [], 'physics': [], 'biology': [], 'chemistry': [], 'history': []}
+        self.s1.grades = {'math': [3, 4], 'English': [], 'physics': [], 'biology': [], 'chemistry': [], 'history': []}
 
 
     def test_check_name(self):
@@ -19,8 +19,14 @@ class TestStudent(unittest.TestCase):
                          ['math', 'English', 'physics', 'biology', 'chemistry', 'history'])
 
 
-    # def test_average_grade(self):
-    #     self.assertEqual(self.average_grade(), 3.5)
+    def test_add_grades(self):
+        self.s1.add_grades('math', 2)
+        self.assertEqual(self.s1.grades, {'math': [3, 4, 2], 'English': [], 'physics': [], 'biology': [], 'chemistry': [], 'history': []})
+
+
+    def test_average_grade(self):
+        self.assertEqual(self.s1.average_grade(), 3.5)
+
 
 
 if __name__ == '__main__':
